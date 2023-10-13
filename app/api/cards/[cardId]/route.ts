da19edd179b4cb9994a47ccf8a0a23beaa64f5d8
Dindault Lucas <dindault.lucas@gmail.com>
2023-10-13 09:16:22 +0200

    add card API

---
+++ app/api/cards/[cardId]/route.ts
@@ -0,0 +1,30 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function PATCH(req: Request, { params }: { params: { cardId: string }}) {
+  try {
+    const { userId } = auth();
+    const { cardId } = params;
+    const values = await req.json();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+    
+    const card = await db.card.update({
+      where: {
+        id: cardId,
+        userId
+      },
+      data: {
+        ...values,
+      }
+    });
+
+    return NextResponse.json(card);
+  } catch (error) {
+    console.log("[card_ID]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+} 
---
+++ app/api/cards/route.ts
@@ -0,0 +1,26 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const { title } = await req.json();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 })
+    }
+
+    const card = await db.card.create({
+      data: {
+        userId,
+        title
+      }
+    });
+
+    return NextResponse.json(card);
+  } catch (error) {
+    console.log("[BOARD]", error);
+    return new NextResponse("Internal Error", { status: 500 })
+  }
+}
